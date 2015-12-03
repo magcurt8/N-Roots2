25d5d2ae0706bab77febf6cafaf8edf932092dc6
Gabe-Issa <Gabe@dhcp-10-5-133-93.wireless.rochester.edu>
2015-12-03 13:52:08 -0500

    wow

--- cgi-bin/init.py
+++ cgi-bin/init.py
@@ -12,7 +12,7 @@ import os
 
 cookie_string = os.environ.get('HTTP_COOKIE')
 if cookie_string:
-    print "Content-type: text/html"
+	print "Content-type: text/html"
 	print "Location:        mynroots.com/planner.html"
 	print # don't forget newline
 	print "<html>"

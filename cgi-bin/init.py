8a623a03c867d41c594ea5bf94e9af2c4c06d866
Gabe-Issa <Gabe@dhcp-10-5-133-93.wireless.rochester.edu>
2015-12-03 13:42:11 -0500

    forgot to change the script to go to the right page in server

--- cgi-bin/init.py
+++ cgi-bin/init.py
@@ -13,7 +13,7 @@ import os
 cookie_string = os.environ.get('HTTP_COOKIE')
 if cookie_string:
     print "Content-type: text/html"
-	print "Location:        mynroots.com/homepage.html"
+	print "Location:        mynroots.com/planner.html"
 	print # don't forget newline
 	print "<html>"
 	print "<body>"
